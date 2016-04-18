eb076948796667cd2cc14e02a36e3e4904f7efb7
sundeqi <dsun@actiontec.com>
2016-04-18 14:33:33 +0800

    故事贴升级Meteor1.3.1

--- hotShareMobile/mobile-config.js
+++ hotShareMobile/mobile-config.js
@@ -30,12 +30,12 @@ App.accessRule('http://*');
 App.accessRule('https://*');
 
 App.icons({
-  'iphone': 'resource/icon_57.png',
+  //'iphone': 'resource/icon_57.png',
   'iphone_2x': 'resource/icon_120.png',
   'iphone_3x': 'resource/icon_180.png',
   'ipad': 'resource/icon_76.png',
   'ipad_2x': 'resource/icon_152.png',
-  'android_ldpi': 'resource/icon_36.png',
+ // 'android_ldpi': 'resource/icon_36.png',
   'android_mdpi': 'resource/icon_48.png',
   'android_hdpi': 'resource/icon_96.png',
   'android_xhdpi': 'resource/icon.png'
@@ -55,20 +55,20 @@ App.icons({
 //    'android_xhdpi_portrait': 'resource/splashEN.png'
 //  });
   App.launchScreens({
-    'iphone': 'resource/splash_768_1024.png',
+    //'iphone': 'resource/splash_768_1024.png',
     'iphone_2x': 'resource/splash_640_960.png',
     'iphone5': 'resource/splash_640_1136.png',
     'iphone6': 'resource/splash_750_1334.png',
     'iphone6p_portrait': 'resource/splash_1242_2208.png',
     'ipad_portrait': 'resource/splash_768_1024.png',
     'ipad_portrait_2x': 'resource/splash_1536_2048.png',
-    'android_ldpi_portrait': 'resource/splash.png',
+    //'android_ldpi_portrait': 'resource/splash.png',
     'android_mdpi_portrait': 'resource/splash.png',
     'android_hdpi_portrait': 'resource/splash.png',
     'android_xhdpi_portrait': 'resource/splash.png'
   });
 
-App.configurePlugin('nl.x-services.plugins.socialsharing', {
+App.configurePlugin('cordova-plugin-x-socialsharing', {
     APP_ID: 'wxcfcf19c225a36351'
 });
 
