574248fb7e85b70031d4fef142c37bd93a142eb6
Gui Fubo <sraita@users.noreply.github.com>
2017-04-18 17:48:06 +0800

    Bump Version to 1.5.14 (#1501)

--- hotShareMobile/lib/6_version.js
+++ hotShareMobile/lib/6_version.js
@@ -1,1 +1,1 @@
-version_of_build = '1.5.12';
+version_of_build = '1.5.14';
--- hotShareMobile/mobile-config.js
+++ hotShareMobile/mobile-config.js
@@ -1,6 +1,6 @@
 App.info({
   id: 'org.hotshare.everywhere',
-  version: '1.5.12',
+  version: '1.5.14',
   name: 'hotShare',
   description: 'Share everything with everyone',
   author: 'hotShare Design Team',
