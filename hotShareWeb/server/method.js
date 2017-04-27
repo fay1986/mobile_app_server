8fbecd6f6637c9abc438558203919217642304c8
Gui Fubo <sraita@users.noreply.github.com>
2017-04-27 16:05:21 +0800

    修复：（监控添加话题功能）重复添加了几次同一个话题，话题里面的帖子也重复了 (#1603)

--- hotShareWeb/server/method.js
+++ hotShareWeb/server/method.js
@@ -1,5 +1,20 @@
 if (Meteor.isServer) {
   Meteor.startup(function () {
+    Array.prototype.minus = function (arr) {
+        var result = new Array();
+        var obj = {};
+        for (var i = 0; i < arr.length; i++) {
+            obj[arr[i]] = 1;
+        }
+        for (var j = 0; j < this.length; j++) {
+            if (!obj[this[j]])
+            {
+                obj[this[j]] = 1;
+                result.push(this[j]);
+            }
+        }
+        return result;
+    };
     Meteor.methods({
       'addTopicsAtReview': function (postId, topicIds) {
         Meteor.defer(function () {
@@ -7,6 +22,7 @@ if (Meteor.isServer) {
             if (postId && topicIds && topicIds.length > 0) {
               var post = Posts.findOne({ _id: postId });
               var topicPosts = TopicPosts.find({ postId: postId }).fetch();
+              var topicPostsIds = [];
               var topicPostObj = {
                 postId: post._id,
                 title: post.title,
@@ -21,13 +37,11 @@ if (Meteor.isServer) {
                 createdAt: new Date()
               };
               for (var i = 0; i < topicPosts.length; i++) {
-                for (var j = 0; j < topicIds; j++) {
-                  if (topicPosts[i].topicId === topicIds[j]) {
-                    topicIds.splice(j, 1)
-                  }
-                }
+                topicPostsIds.push(topicPosts[i].topicId);
               }
 
+              topicIds = topicIds.minus(topicPostsIds);
+              
               for (var k = 0; k < topicIds.length; k++) {
                 topicPostObj.topicId = topicIds[k];
                 TopicPosts.insert(topicPostObj)
