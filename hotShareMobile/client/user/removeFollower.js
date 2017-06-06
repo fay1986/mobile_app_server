9bc68423012c05abab8ef2c6eaf417000e606ee2
Simba Zhang <solderzzc@gmail.com>
2017-06-06 14:45:58 -0700

    The id is collection id, but not followerId. To fix it, need find the FollowerId in the follower collection on the client. #1879.

--- hotShareMobile/client/user/removeFollower.js
+++ hotShareMobile/client/user/removeFollower.js
@@ -3,9 +3,13 @@
  */
 
 if(Meteor.isClient){
-    removeFollower = function(followerId){
+    removeFollower = function(_id){
         try{
-            Follower.remove(followerId)
+            var follower = Follower.findOne({_id:_id},{fields:{followerId:true}})
+            Follower.remove(_id)
+            if(follower && follower.followerId){
+                FollowPosts._collection.remove({owner:follower.followerId})
+            }
         } catch(e){
             console.log(e)
             console.log('Exception when remove follower on client')
